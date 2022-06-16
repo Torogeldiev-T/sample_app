087d1b758f606827e6bd2736bbfb90b2e20220ff
Tilek <tilek2402@gmail.com>
2022-06-16 14:28:14 +0600

    Make a basic User model (including secure passwords)

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -1,23 +1,20 @@
-require "test_helper"
+require 'test_helper'
 class SiteLayoutTest < ActionDispatch::IntegrationTest
   # test "the truth" do
   #   assert true
   # end
-  test "layout links" do
+  test 'layout links' do
     get root_path
     assert_template 'static_pages/home'
-    assert_select "a[href=?]", root_path, count: 2
-    assert_select "a[href=?]", help_path
-    assert_select "a[href=?]", about_path
-    assert_select "a[href=?]", contact_path
-
+    assert_select 'a[href=?]', root_path, count: 2
+    assert_select 'a[href=?]', help_path
+    assert_select 'a[href=?]', about_path
+    assert_select 'a[href=?]', contact_path
   end
-  
-  test "visit sign up page" do
+
+  test 'visit sign up page' do
     get signup_path
     assert_template 'users/new'
     assert_select 'title', 'Sign Up | Ruby on Rails Tutorial Sample App'
-  
   end
 end
-
