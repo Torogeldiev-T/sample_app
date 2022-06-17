d8a21461bfe5dd60d26fe3ebfd4276fae60b6750
Tilek <tilek2402@gmail.com>
2022-06-17 10:15:59 +0600

    Finish user signup

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,11 @@
 Rails.application.routes.draw do
-  get 'users/new', as: 'signup'
-  root "static_pages#home"
-  get  "/about", to: "static_pages#about"
-  get  "/help", to: "static_pages#help"
-  get  "/contact", to: "static_pages#contact"
-
+  root 'static_pages#home'
+  get  '/about', to: 'static_pages#about'
+  get  '/help', to: 'static_pages#help'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
+  resources :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  
 end
---
+++ test/integration/user_signup_test.rb
@@ -0,0 +1,28 @@
+require 'test_helper'
+
+class UserSignupTest < ActionDispatch::IntegrationTest
+  test 'invalid signup path' do
+    get signup_path
+    assert_no_difference 'User.count' do
+      post users_path, params: { user: { name: '',
+                                         email: 'user@valid.com',
+                                         password: 'foobar',
+                                         password_confirmation: 'foobar' } }
+    end
+    assert_response :unprocessable_entity
+    assert_template 'users/new'
+    assert_select 'div.alert.alert-danger', 'The form contains 1 error'
+  end
+
+  test 'valid signup path' do
+    assert_difference 'User.count', 1 do
+      post users_path, params: { user: { name: 'Valid Name',
+                                         email: 'exmpl@valid.com',
+                                         password: 'foobar',
+                                         password_confirmation: 'foobar' } }
+    end
+    follow_redirect!
+    assert_template 'users/show'
+    assert flash[:success]
+  end
+end
