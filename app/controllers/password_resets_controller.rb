5b474e4906c0265d6c62c3f4e630407b75bae637
Tilek <tilek2402@gmail.com>
2022-06-22 16:49:11 +0600

    Password reset finished

--- app/controllers/password_resets_controller.rb
+++ app/controllers/password_resets_controller.rb
@@ -1,4 +1,7 @@
 class PasswordResetsController < ApplicationController
+  before_action :get_user,   only: %i[edit update]
+  before_action :valid_user, only: %i[edit update]
+  before_action :check_expiration, only: %i[edit update]
   def new; end
 
   def edit; end
@@ -13,6 +16,50 @@ class PasswordResetsController < ApplicationController
     else
       flash.now[:danger] = 'Email address not found'
       render 'new', status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if params[:user][:password].empty?
+      @user.errors.add(:password, "can't be empty")
+      render 'edit', status: :unprocessable_entity
+    elsif @user.update(user_params)
+      # Security solution against session hijaking attack
+      @user.forget
+
+      reset_session
+      log_in @user
+      # Security solution to assgn nil to reset dijest in order to protect from changing password again through the same link
+      @user.update_attribute(:reset_digest, nil)
+      flash[:success] = 'Password has been reset'
+      redirect_to @user
+    else
+      render 'edit', status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:password, :password_confirmation)
+  end
+
+  def get_user
+    @user = User.find_by(email: params[:email])
+  end
+
+  # Confirms a valid user.
+  def valid_user
+    unless @user && @user.activated? &&
+           @user.authenticated?(:reset, params[:id])
+      redirect_to root_url
+    end
+  end
+
+  def check_expiration
+    if @user.password_reset_expired?
+      flash[:danger] = 'Password reset has expired'
+      redirect_to new_password_reset_url
     end
   end
 end
